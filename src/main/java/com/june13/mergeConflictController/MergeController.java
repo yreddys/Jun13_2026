22f09295e7bc2350bfaa4d7db79c1c4eb45bbc94
yreddys <yreddys211@gmail.com>
2026-06-13 16:54:23 +0530

    new feature controller added

--- src/main/java/com/june13/mergeConflictController/MergeController.java
+++ src/main/java/com/june13/mergeConflictController/MergeController.java
@@ -1,4 +1,12 @@
 package com.june13.mergeConflictController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MergeController {
+    @GetMapping("/merge")
+    public String mergeTest(){
+        return "hello from merge";
+    }
 }
