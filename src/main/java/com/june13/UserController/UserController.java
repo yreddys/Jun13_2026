f9d6f26595cb699c90acb9b63bbc1a42bd74ebec
yreddys <yreddys211@gmail.com>
2026-06-13 17:43:31 +0530

    new userController added

---
+++ src/main/java/com/june13/UserController/UserController.java
@@ -0,0 +1,19 @@
+package com.june13.UserController;
+
+import com.june13.entity.User;
+import com.june13.userService.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/saveUser")
+    public String saveUser(@RequestBody User user){
+        userService.create(user);
+          return "saved successfully";
+    }
+}
---
+++ src/main/java/com/june13/entity/User.java
@@ -0,0 +1,50 @@
+package com.june13.entity;
+
+public class User {
+
+    private int userId;
+    private String userName;
+
+    // Default Constructor
+    public User() {
+    }
+
+    // Parameterized Constructor
+    public User(int userId) {
+        this.userId = userId;
+    }
+
+    // Parameterized Constructor
+    public User(int userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    // Getter
+    public int getUserId() {
+        return userId;
+    }
+
+    // Setter
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    // Getter
+    public String getUserName() {
+        return userName;
+    }
+
+    // Setter
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
